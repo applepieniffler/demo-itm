7057134e3360e00b3e3282b923db5dc5899bacbb
Annika Gilles <annikagilles@Annikas-MacBook-Air.local>
2023-07-18 14:24:49 +0800

    last

--- yt.py
+++ yt.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
 
-yt = YouTube('https://www.youtube.com/watch?v=m9Hgi90LTv8')
+yt = YouTube('https://www.youtube.com/watch?v=TyKKKgf7YcM')
 
 yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
